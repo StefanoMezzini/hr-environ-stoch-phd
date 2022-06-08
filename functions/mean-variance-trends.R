ea3ff5d68504bff0e9f7cb4fa264213acd9ec188
Stefano Mezzini <stefano@menouno.com>
2022-06-08 10:39:10 -0700

    streamlined mean-variance functions

--- functions/mean-variance-trends.R
+++ functions/mean-variance-trends.R
@@ -1,85 +1,86 @@
 library('dplyr') # for data wrangling (e.g., %>%) and tibble()
 
 # mean functions
+min_mu <- 10
+max_mu <- 100
+
 mean_constant <- function(generations) {
-  tibble(mu = rep(10, length(generations))) %>%
-    list() %>%
-    return()
+  rep(mean(c(min_mu, max_mu)), length(generations))
 }
 
 mean_linear <- function(generations) {
-  tibble(mu = generations * 0.045 + 6) %>%
-    list() %>%
-    return()
+  b <- (max_mu - min_mu) / (200 - 1)
+  
+  generations * b + min_mu - b
 }
 
 mean_cyclical <- function(generations) {
-  tibble(mu = sin(generations / 10) * 4.5 + 10.5) %>%
-    list() %>%
-    return()
+  sin(generations / 10) * (max_mu - min_mu) / 2 + mean(c(min_mu, max_mu))
 }
 
 mean_drifting <- function(generations, seed = 8) {
   set.seed(seed = seed)
-  tibble(mu = cumsum(rnorm(generations, sd = 0.5))) %>%
-    mutate(mu = mu - min(mu) + 6) %>%
-    list() %>%
-    return()
+  mu <- cumsum(rnorm(length(generations)))
+  mu <- mu - min(mu) # set minimum to 0
+  mu <- mu / max(mu) # shrink the range to [0, 1]
+  mu <- mu * (max_mu - min_mu) # scale to the range of mu
+  mu <- mu + min_mu # shift to the right intercept
+  return(mu)
 }
 
 mean_erratic <- function(generations) {
-  tibble(mu = case_when(generations < quantile(generations, 0.25) ~ 7,
-                        generations < quantile(generations, 2/3) ~ 3,
-                        TRUE ~ 10) + 5) %>%
-    list() %>%
-    return()
+  case_when(generations < quantile(generations, 0.25)~mean(c(min_mu, max_mu)),
+                        generations < quantile(generations, 2/3) ~ min_mu,
+                        TRUE ~ max_mu)
 }
 
 # variance functions
+min_s2 <- 5
+max_s2 <- 200
+
 variance_constant <- function(generations) {
-  tibble(sigma2 = rep(25, length(generations))) %>%
-    list() %>%
-    return()
+  rep(mean(c(min_s2, max_s2)), length(generations))
 }
 
 variance_linear <- function(generations) {
-  tibble(sigma2 = generations * 0.2 + 10) %>%
-    list() %>%
-    return()
+  b <- (max_s2 - min_s2) / (200 - 1)
+  
+  generations * b + min_s2 - b
 }
 
 variance_cyclical <- function(generations) {
-  tibble(sigma2 = (sin(generations / 15) + 1)^3 * 5 + 10) %>%
-  list() %>%
-  return()
+  ((sin(generations / 15) + 1)/2)^3 * (max_s2 - min_s2) + min_s2
 }
 
 variance_drifting <- function(generations, seed = 1) {
   set.seed(seed)
-  tibble(sigma2 = cumsum(rnorm(length(generations), sd = 2.5)) %>% abs() + 10) %>%
-    list() %>%
-    return()
+  sigma2 <- length(generations) %>%
+    rnorm() %>%
+    cumsum() %>%
+    abs()
+  sigma2 <- sigma2 - min(sigma2) # ranges [0, Inf)
+  sigma2 <- sigma2 / max(sigma2) # to [0, 1]
+  sigma2 <- sigma2 * (max_s2 - min_s2) + min_s2 # to [min_s2, max_s2]
+  return(sigma2)
 }
 
 variance_erratic <- function(generations) {
-  tibble(sigma2 = case_when(generations < quantile(generations, 0.2) ~ 35,
-                            generations < quantile(generations, 0.6) ~ 50,
-                            TRUE ~ 15)) %>%
-  list() %>%
-  return()
+  case_when(generations < quantile(generations, 0.2) ~ (max_s2 + min_s2) / 1.75,
+                     generations < quantile(generations, 0.6) ~ max_s2,
+                     TRUE ~ min_s2)
 }
 
 # check ranges of functions
 if(FALSE) {
-  mean_constant(1:200)[[1]]$mu %>% unique()
-  mean_linear(c(1, 200))[[1]]$mu
-  mean_cyclical(1:200)[[1]]$mu %>% range()
-  mean_drifting(1:200)[[1]]$mu %>% range()
-  mean_erratic(1:200)[[1]]$mu %>% range()
+  mean_constant(1:200) %>% unique()
+  mean_linear(c(1, 200))
+  mean_cyclical(1:200) %>% range()
+  mean_drifting(1:200) %>% range()
+  mean_erratic(1:200) %>% range()
   
-  variance_constant(1:200)[[1]]$sigma2 %>% unique()
-  variance_linear(c(1, 200))[[1]]$sigma2
-  variance_cyclical(1:200)[[1]]$sigma2 %>% range()
-  variance_drifting(1:200)[[1]]$sigma2 %>% range()
-  variance_erratic(1:200)[[1]]$sigma2 %>% range()
+  variance_constant(1:200) %>% unique()
+  variance_linear(c(1, 200))
+  variance_cyclical(1:200) %>% range()
+  variance_drifting(1:200) %>% range()
+  variance_erratic(1:200) %>% range()
 }
