98387e6f1ff0750ecec0c9d93cf966fe01e2bdb6
Stefano Mezzini <stefano@menouno.com>
2022-08-24 11:31:59 -0700

    added parameter definition in comments

--- functions/get_hr.R
+++ functions/get_hr.R
@@ -1,3 +1,5 @@
 #' function to get home range estimate starting from positional variance (`sigma`)
 #' **NOTE:** only works when AKDE is isotropic and Gaussian!
+#' @param .sigma positional variance estimated by a CTMM model
+#' @param quantile quantile within the interval (0, 1) for which HR should be calculated
 get_hr <- function(.sigma, quantile) -2 * log(1 - quantile) * pi * .sigma
