4025b6177f531600d6458c8b170accdbfe866fa5
Stefano Mezzini <stefano@menouno.com>
2022-08-24 13:46:48 -0700

    renamed moving window function for home ranges

---
+++ functions/window_hr.R
@@ -0,0 +1,139 @@
+# function that loops through a moving window of X days of tracking data
+# computes estimated HR size and CIs for each window
+# plots ML point estimate of HR size and 95% CIs
+# written by Stefano Mezzini based on code by Mark Bidwell and Chris Fleming
+
+library('ctmm')  # for continuous movement modeling
+library('dplyr') # for data wrangling
+library('purrr') # for functional programming
+
+window_hr <- function(tel, window, dt, projection, fig_path = NULL, rds_path = NULL) {
+  
+  # moving window created at the beginning of t; slides forward as far as possible:
+  # |---|..... --> .|---|.... --> ..|---|... --> ...|---|.. --> ....|---|. --> .....|---|
+  
+  times <- seq(min(tel$t), max(tel$t) - window, by = dt)
+  N <- length(times)
+  date_range <- range(times)
+  
+  # to extract home ranges later
+  extract_hr <- function(a, par, l.ud) {
+    summary(a, units = FALSE, level.UD = l.ud)$CI['area (square meters)', ][par] / 1e6
+  }
+  
+  out <-
+    tibble(
+      # add start and end times
+      t_start = times, # left bound
+      t_end = t_start + window, # left bound
+      # subset times within window (t to t + window; can't use filter() bc telemetry)
+      dataset = map2(t_start, t_end, \(t_1, t_2) tel[tel$t >= t_1 & tel$t <= t_2, ]),
+      models =
+        imap(dataset,
+             \(d, i) {
+               
+               if(nrow(d) > 1) {
+                 cat('Analyzing dataset ', i, ' of ', N, '.\n', sep = '')
+                 tibble(
+                   # find initial guesses for models
+                   guess = ctmm.guess(data = d, interactive = FALSE) %>% list(),
+                   # select best model based on subset of tel
+                   model = ctmm.select(data = d, CTMM = guess[[1]]) %>% list(),
+                   # estimate autocorrelated kernel density estimate
+                   akde = akde(data = d, CTMM = model[[1]]) %>% list(),
+                   # find home range estimate
+                   hr_est_50 = extract_hr(a = akde[[1]], par = 'est', l.ud = 0.50),
+                   hr_lwr_50 = extract_hr(a = akde[[1]], par = 'low', l.ud = 0.50),
+                   hr_upr_50 = extract_hr(a = akde[[1]], par = 'high', l.ud = 0.50),
+                   hr_est_95 = extract_hr(a = akde[[1]], par = 'est', l.ud = 0.95),
+                   hr_lwr_95 = extract_hr(a = akde[[1]], par = 'low', l.ud = 0.95),
+                   hr_upr_95 = extract_hr(a = akde[[1]], par = 'high', l.ud = 0.95))
+               } else {
+                 tibble(
+                   # find initial guesses for models
+                   guess = list('Insufficient data.'),
+                   # select best model based on subset of tel
+                   model =list('Insufficient data.'),
+                   # estimate autocorrelated kernel density estimate
+                   akde =list('Insufficient data.'),
+                   # find home range estimate
+                   hr_est_50 = NA_real_,
+                   hr_lwr_50 = NA_real_,
+                   hr_upr_50 = NA_real_,
+                   hr_est_95 = NA_real_,
+                   hr_lwr_95 = NA_real_,
+                   hr_upr_95 = NA_real_)
+               } # close else
+             })) %>% # close function for imap()
+    unnest(models) %>%
+    mutate(t_center = (t_start + t_end) / 2,
+           posixct = as.POSIXct(t_center, origin = '1970-01-01', tz = tel@info$timezone),
+           date = as.Date(posixct))
+  
+  if(! is.null(rds_path)) {
+    saveRDS(out, file.path(rds_path, paste0(tel@info['identity'],
+                                            '-window-', window / (1 %#% 'day'), '-days',
+                                            '-dt-', dt / (1 %#% 'day'), '-days.rds')))
+  }
+  
+  # plot results
+  library('ggplot2') # for fancy plots
+  theme_set(theme_bw() + theme(legend.position = 'none'))
+  
+  # tracking data
+  plt_a <-
+    ggplot(tel) +
+    coord_equal() +
+    geom_point(aes(longitude, latitude, color = timestamp)) +
+    geom_path(aes(longitude, latitude), alpha = 0.1) +
+    scale_color_viridis_c('Time') +
+    labs(x = '', y = NULL)
+  
+  plt_b <-
+    ggplot(out) +
+    # core home range
+    geom_ribbon(aes(date, ymin = hr_lwr_50, ymax = hr_upr_50), fill = 'grey') +
+    geom_line(aes(date, hr_est_50), size = 1.25) +
+    geom_line(aes(date, hr_est_50, color = posixct)) +
+    
+    # 95% home range
+    geom_ribbon(aes(date, ymin = hr_lwr_95, ymax = hr_upr_95), fill = 'grey') +
+    geom_line(aes(date, hr_est_95), size = 1.25) +
+    geom_line(aes(date, hr_est_95, color = posixct)) +
+    
+    scale_x_date(NULL, date_labels = '%b %Y') +
+    scale_color_viridis_c() +
+    labs(y = expression(Home~range~(m^2)))
+  
+  plt <- cowplot::plot_grid(plt_a, plt_b, labels = c('a.', 'b.'), nrow = 1, align = 'hv')
+  
+  if (! is.null(fig_path)) {
+    # Save figure as a png using the animal's name
+    ggsave(filename = file.path(fig_path, paste0(tel@info['identity'],
+                                                 '-window-', window/(1%#%'day'), '-days',
+                                                 '-dt-', dt / (1%#% 'day'), '-days.png')),
+           plot = plt, units = 'in', width = 7, height = 3,
+           dpi = 600, bg = 'white')
+  } else {
+    print(plt)
+  }
+  # return(out)
+}
+
+if(FALSE) {
+  # Test the function on the buffalo data
+  data('buffalo')
+  
+  # Apply to a single individual
+  test <- window_hr(tel = buffalo$Queen, # buffalo with smallest dataset
+                    window = 7 %#% 'day', # size of the moving window
+                    dt = 1 %#% 'day', # step size for the moving window
+                    fig_path = NULL) # can specify where to save the figure
+  
+  # apply to all buffaloes in the dataset
+  results <- lapply(buffalo,
+                    window_hr,
+                    # arguments to pass to window_hr
+                    window = 30 %#% 'day',
+                    dt = 2 %#% 'day')
+}
