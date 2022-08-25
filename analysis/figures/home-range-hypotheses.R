b49471218c0813b842db820adcb9d3b18f6c16e2
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-08-25 14:49:15 -0700

    added notes to hypothesis figures

--- analysis/figures/home-range-hypotheses.R
+++ analysis/figures/home-range-hypotheses.R
@@ -2,6 +2,7 @@ library('ctmm')    # for movement modeling
 library('dplyr')   # for data wrangling
 library('tidyr')   # for data wrangling
 library('ggplot2') # for fancy plots
+library('ggbrace') # for curly braces on fancy plots
 library('cowplot') # for fancy multi-panel plots
 source('analysis/figures/default-figure-styling.R') # for common theme and color palette
 
@@ -9,7 +10,41 @@ theme_set(theme_get() + theme(legend.position = 'none'))
 
 hr_lab <- '\U1D54D(position)' # label for y axis
 
-# simulate animal movement ----
+# hypothesis for change in H over E(R) ----
+d <- tibble(mu = seq(0, 1, length.out = 400),
+            sigma2 = rev(mu),
+            split = mu < 0.35,
+            h_1 = if_else(mu < 0.2, 2, sinpi(0.5 - (mu - 0.2) / 2)^10 * 2) + 0.2,
+            h_2 = if_else(split, 2 * mu^2 - 7 * mu + 3.916873, h_1),
+            h_3 = if_else(split, h_2 + 30 * (0.35 - mu)^2, h_2))
+
+notes <- tibble(x = c(0.13, 0.36, 0.175, 0.68),
+                y = c(2.05, 3.2, 1.35, 2.05),
+                text = c('range-resident', 'nomadic or dispersing',
+                         'evolutionary timescale', 'ecological timescale'))
+
+p_mu <-
+  ggplot() +
+  coord_cartesian(ylim = c(0, 4)) +
+  geom_ribbon(aes(mu, ymin = h_1, ymax = h_3), d, color = 'black', alpha = 0.2) +
+  geom_brace(aes(x = c(0, 0.345), y = c(1.45, 1.7)), rotate = 180) +
+  geom_brace(aes(x = c(0.36, 1), y = c(1.95, 1.7)), rotate = 0) +
+  geom_text(aes(x = x, y = y, label = text), notes) +
+  scale_x_continuous('\U1D53C(\U1D445)', breaks = NULL, expand = c(0, 0)) +
+  scale_y_continuous(hr_lab, breaks = 0); p_mu
+
+# hypothesis for change in H over V(R) ----
+p_s2 <-
+  p_mu +
+  scale_x_reverse('\U1D54D(\U1D445)', breaks = NULL, expand = c(0, 0)); p_s2
+
+# save the plots ----
+ggsave('figures/mean-abundance-hr-hypotheses.png', plot = p_mu, width = 4, height = 4,
+       scale = 1.5, dpi = 400, bg = 'white')
+ggsave('figures/variance-abundance-hr-hypotheses.png', plot = p_s2, width = 4, height = 4,
+       scale = 1.5, dpi = 400, bg = 'white')
+
+# add simulated animal movement ----
 p_track <-
   simulate(ctmm(tau = c(Inf, 1), sigma = 1), t = seq(1, 400, by = 0.05), seed = 5) %>%
   SpatialPoints.telemetry() %>%
@@ -21,37 +56,6 @@ p_track <-
   scale_y_continuous(NULL, breaks = NULL) +
   theme(plot.background = element_blank())
 
-# hypothesis for change in H over E(R) ----
-d <- tibble(mu = seq(0, 1, length.out = 400),
-            sigma2 = rev(mu),
-            split = mu < 0.35,
-            h_1 = if_else(mu < 0.2, 2, sinpi(0.5 - (mu - 0.2) / 2)^10 * 2) + 0.2,
-            h_2 = if_else(split, 2 * mu^2 - 7 * mu + 3.916873, h_1),
-            h_3 = if_else(split, h_2 + 30 * (0.35 - mu)^2, h_2))
-
-p_mu <-
-  ggplot(d, aes(mu)) +
-  coord_cartesian(ylim = c(0, 4)) +
-  geom_ribbon(aes(ymin = h_1, ymax = h_3), alpha = 0.3) +
-  geom_line(aes(y = h_1)) +
-  geom_line(aes(y = h_3)) +
-  labs(x = '\U1D53C(\U1D445)', y = hr_lab) +
-  scale_x_continuous(breaks = NULL, expand = c(0, 0)) +
-  scale_y_continuous(breaks = 0)
-
-# hypothesis for change in H over V(R) ----
-# vary color of lines by certainty ([0, 1]), with 1 being black and 0 being black w 0% alpha
-p_s2 <-
-  ggplot(d, aes(sigma2)) +
-  coord_cartesian(ylim = c(0, 4)) +
-  geom_ribbon(aes(ymin = h_1, ymax = h_3), alpha = 0.3) +
-  geom_line(aes(y = h_1)) +
-  geom_line(aes(y = h_3)) +
-  labs(x = '\U1D54D(\U1D445)', y = hr_lab) +
-  scale_x_continuous(breaks = NULL, expand = c(0, 0)) +
-  scale_y_continuous(breaks = 0)
-
-# add the animal tracks ---
 p_e <-
   ggdraw(p_mu) +
   draw_plot(p_track, x = 1, y = 1.0335, width = 0.45, height = 0.45, hjust = 1, vjust = 1)
@@ -60,9 +64,3 @@ p_v <-
   ggdraw(p_s2) +
   draw_plot(p_track, x = 0.096, y = 1.0335, width = 0.45, height = 0.45, hjust = 0,
             vjust = 1)
-
-# save the plots
-ggsave('figures/mean-abundance-hr-hypotheses.png', plot = p_mu, width = 4, height = 4,
-       dpi = 600, bg = 'white')
-ggsave('figures/variance-abundance-hr-hypotheses.png', plot = p_s2, width = 4, height = 4,
-       dpi = 600, bg = 'white')
