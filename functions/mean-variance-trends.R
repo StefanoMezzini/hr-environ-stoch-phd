24a24bc8c55079aeedc0d814380e24413994f386
Stefano Mezzini <stefano@menouno.com>
2022-09-22 07:50:32 -0700

    created final version of functions; added comments

--- functions/mean-variance-trends.R
+++ functions/mean-variance-trends.R
@@ -1,8 +1,19 @@
 library('dplyr') # for data wrangling (e.g., %>%) and tibble()
 
 # mean functions
-min_mu <- 10
-max_mu <- 100
+# mean should be far from zero because a small mean with large variance implies the scale
+# parameter is very large, which results in a heavy right tail and a large range in the
+# simulated values.
+# This issue would be clearer when parameterizing in terms of shape and scale, but the
+# parameterization is often harder to plot and visualize.
+min_mu <- 100
+max_mu <- 200
+
+#' animals reach their `REQUIRED` amount of energy after eating many times: 
+#' `REQUIRED` should be high enough that all animals are forced to eat multiple times
+#' `REQUIRED` should *not* be so high that HR estimates become too variable
+#' the variance in HR depends on the variance in total food, i.e. sum of variances
+REQUIRED <- max_mu * 10
 
 mean_constant <- function(generations) {
   rep(mean(c(min_mu, max_mu)), length(generations))
@@ -30,13 +41,13 @@ mean_drifting <- function(generations, seed = 8) {
 
 mean_erratic <- function(generations) {
   case_when(generations < quantile(generations, 0.25)~mean(c(min_mu, max_mu)),
-                        generations < quantile(generations, 2/3) ~ min_mu,
-                        TRUE ~ max_mu)
+            generations < quantile(generations, 2/3) ~ min_mu,
+            TRUE ~ max_mu)
 }
 
 # variance functions
-min_s2 <- 5
-max_s2 <- 200
+min_s2 <- 50^2
+max_s2 <- 500^2
 
 variance_constant <- function(generations) {
   rep(mean(c(min_s2, max_s2)), length(generations))
@@ -66,21 +77,43 @@ variance_drifting <- function(generations, seed = 1) {
 
 variance_erratic <- function(generations) {
   case_when(generations < quantile(generations, 0.2) ~ (max_s2 + min_s2) / 1.75,
-                     generations < quantile(generations, 0.6) ~ max_s2,
-                     TRUE ~ min_s2)
+            generations < quantile(generations, 0.6) ~ max_s2,
+            TRUE ~ min_s2)
 }
 
 # check ranges of functions
 if(FALSE) {
-  mean_constant(1:200) %>% unique()
-  mean_linear(c(1, 200))
-  mean_cyclical(1:200) %>% range()
-  mean_drifting(1:200) %>% range()
-  mean_erratic(1:200) %>% range()
+  library('purrr')   # for functional programming
+  library('ggplot2') # for fancy plots
+  source('functions/rgamma2.R') # rgamma() parameterized by mean and variance
+  theme_set(theme_bw())
+  expand.grid(animal = 1:200,
+              mean = c('constant', 'linear', 'cyclical', 'drifting', 'erratic'),
+              variance = c('constant', 'linear', 'cyclical', 'drifting', 'erratic')) %>%
+    mutate(mu = purrr::map2_dbl(mean, animal, \(.mean, .animal) {
+      foo <- get(paste0('mean_', .mean))
+      foo(1:200)[.animal]
+    }),
+    sigma2 = purrr::map2_dbl(variance, animal, \(.variance, .animal) {
+      foo <- get(paste0('variance_', .variance))
+      foo(1:200)[.animal]
+      }),
+    cv = sqrt(sigma2) / mu) %>%
+    ggplot(aes(animal, cv)) +
+    facet_grid(variance ~ mean) +
+    geom_line() +
+    scale_x_continuous(sec.axis = sec_axis(trans = 'identity',
+                                           name = 'Mean', breaks = NULL)) +
+    scale_y_continuous(limits = c(0, NA),
+                       sec.axis = sec_axis('identity', 'Variance', NULL))
   
-  variance_constant(1:200) %>% unique()
-  variance_linear(c(1, 200))
-  variance_cyclical(1:200) %>% range()
-  variance_drifting(1:200) %>% range()
-  variance_erratic(1:200) %>% range()
+  tidyr::expand_grid(mu = c(min_mu, max_mu),
+                     s2 = c(min_s2, max_s2)) %>%
+    mutate(cv = sqrt(s2) / mu,
+           k = s2 / mu^2, # shape parameter
+           theta = s2 / mu, # scale parameter
+           sample = map2(mu, s2, \(..x, ..y) rgamma2(mu = ..x, sigma2 = ..y, N = 1e6)),
+           min = map_dbl(sample, min),
+           max = map_dbl(sample, max),
+           range = map_dbl(sample, \(x) range(x) %>% diff()))
 }
