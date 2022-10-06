f95c99605ffaf76caea5afd877da17964ef635f8
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 16:32:11 -0700

    changed to 1024 tracks, added comments

--- analysis/simulations/tracks.R
+++ analysis/simulations/tracks.R
@@ -6,10 +6,7 @@ library('purrr')  # for functional programming
 source('functions/label_visits.R') # function for when animal moves to a new cell & eats
 source('analysis/simulations/movement-model.R') # for consistency between scripts
 
-# track progress on a file
-cat('', file = 'analysis/sockeye-scripts/progress.txt', append = FALSE)
-
-N_DAYS <- 100 # number of "days" (i.e., simulations with different seeds)
+N_DAYS <- 2^10 # number of "days" (i.e., simulations with different seeds)
 SIMULATE_TRACKS <- TRUE # set to "TRUE" if you want to generate new tracks
 
 # extracts simulated tracks from a ctmm movement model for given sample times
@@ -22,8 +19,6 @@ get_tracks <- function(day, times = SAMPLES) {
 }
 
 # generate simulated tracks (will be truncated at satiety later)
-cat('Generating tracks...\n', file = 'analysis/sockeye-scripts/progress.txt',
-    append = TRUE)
 if(SIMULATE_TRACKS) {
   tracks <- tibble(day = 1:N_DAYS, # a simulation for each day
                    tel = map(.x = day, # set a seed for consistent results
@@ -32,8 +27,6 @@ if(SIMULATE_TRACKS) {
 } else tracks <- readRDS('simulations/tracks.rds')
 
 # find patch visits and calories consumed from the tracks
-cat('Labelling tracks...\n', file = 'analysis/sockeye-scripts/progress.txt',
-    append = TRUE)
 tracks <- transmute(tracks, # drop tel column
                     day, # keep day column
                     track = map(.x = tel, # add a column of full tracks
@@ -46,9 +39,6 @@ saveRDS(tracks, file = 'simulations/labelled-tracks.rds')
 # check if grid any animals moved out of the habitat raster
 if(any(is.na(tracks$new_cell))) {
   warning(paste('CAUTION:', sum(tracks$new_cell), 'animals left the habitat raster!'))
-  cat('\n\n* * *\n\n', sum(tracks$new_cell),
-      'animals left the habitat raster!\n\n* * *\n\n',
-      file = 'analysis/sockeye-scripts/progress.txt', append = TRUE)
 }
 
 # check scaling is consistent and sampling frequency is sufficiently high
@@ -67,11 +57,13 @@ if(FALSE) {
   
   track_short <- filter(track, t < 1e4)
   
+  # convert the raster to a data.frame to ensure tracks don't go too far
   test_habitat <- rasterToPoints(HABITAT, spatial = TRUE) %>% data.frame()
   test_habitat_small <- filter(test_habitat,
                                x >= min(track$x), x <= max(track$x),
                                y >= min(track$y), y <= max(track$y))
   
+  # plot a portion of a track
   ggplot(track_short, aes(x, y)) +
     coord_equal(xlim = range(track_short$x), ylim = range(track_short$y)) +
     geom_tile(data = test_habitat_small, fill = 'transparent', color = 'black') +
@@ -81,6 +73,7 @@ if(FALSE) {
     scale_color_brewer('New cell', type = 'qual', palette = 6) +
     scale_size_manual('New cell', values = c(1, 2))
   
+  # plot an entire track
   ggplot(track, aes(x, y)) +
     coord_equal(xlim = range(track$x), ylim = range(track$y)) +
     geom_tile(data = test_habitat, fill = 'transparent', color = 'black') +
@@ -90,6 +83,7 @@ if(FALSE) {
     scale_color_brewer('New cell', type = 'qual', palette = 6) +
     scale_size_manual('New cell', values = c(1, 2))
   
+  # create a tibble of all the tracks
   test <- test %>%
     mutate(tel = map(tel, data.frame)) %>%
     tidyr::unnest(tel) %>%
@@ -99,17 +93,20 @@ if(FALSE) {
              suppressWarnings(),
            new_cell = c(0, diff(new_cell)) != 0)
   
+  # plot all the tracks (can take some time with 1024 tracks...)
   test %>%
     ggplot(aes(x, y)) +
     coord_equal(xlim = range(test$x), ylim = range(test$y)) +
     geom_tile(data = test_habitat, fill = 'transparent', color = 'black') +
     geom_path(aes(group = day), alpha = 0.4)
   
+  # make a histogram of the total number of encounters per track
   test %>%
     group_by(day) %>%
     summarize(encounters = sum(new_cell)) %>%
     ggplot() +
-    geom_histogram(aes(encounters), bins = 7, color = 'black')
+    geom_histogram(aes(encounters), bins = ceiling(log2(N_DAYS)) + 1,
+                   color = 'black')
   
   test %>%
     group_by(day) %>%
