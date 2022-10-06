4caa629c9f5c1eb233261278fc51ad03241ab6a5
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 15:44:32 -0700

    added code to save as single RDS

--- analysis/variance-raster/tapirs/tapir-ndvi-data-to-local.R
+++ analysis/variance-raster/tapirs/tapir-ndvi-data-to-local.R
@@ -5,6 +5,9 @@
 library('dplyr')    # for data wrangling
 library('sf')       # for spatial features
 library('MODIStsp') # for downloading NDVI rasters
+library('purrr')  # for functional programming
+library('tidyr')  # for data wrangling
+library('raster') # to import and save rasters
 source('earthdata-login-info.R') # import personal login credentials for EarthData
 
 # use a bounding box from tracking data ----
@@ -28,30 +31,46 @@ MODIStsp(gui = FALSE, # do not use the browser GUI, only run in R
          sensor = 'Terra', # only terrestrial values, ignore main bodies of water
          user = USERNAME, # your Earthdata username (for urs.earthdata.nasa.gov/home)
          password = PASSWORD, # your Earthdata password
-         start_date = '2000.01.01', # earliest data is 2000-02-18
-         end_date = format(Sys.Date(), '%Y.%m.%d'), # download all available data
-         spatmeth = 'bbox', # see `spafile` argument
+         start_date = format(min(tel$timestamp) - 16, '%Y.%m.%d'), # 1 raster before min
+         end_date = format(max(tel$timestamp) + 16, '%Y.%m.%d'), # 1 raster after end
+         spatmeth = 'bbox', # use a bounding box for the extent
          bbox = bbox, # spatial file for raster extent
          out_projsel = 'User Defined', # use specified projection instead of default
          output_proj = '+proj=longlat', # download unprojected raster
          resampling = 'bilinear', # method for resampling raster if changing projection
          delete_hdf = TRUE, # delete HDF files after download is complete
          scale_val = TRUE, # convert from integers to floats within [-1, 1]
-         ts_format = 'R RasterStack', # also save as an R RasterStack as an Rda file
          out_format = 'GTiff', # output format: 'ENVI' (.hdr) or 'GTiff' (.tif)
          n_retries = 10, # number of times to try again if download fails before aborting
          verbose = TRUE, # print processing messages
-         parallel = TRUE) # use TRUE for automatic number of cores (max 8), or specify
+         parallel = 10) # use TRUE for automatic number of cores (max 8), or specify
 
 # check rasters ----
+ud_poly <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.9995, level = 0) %>%
+  st_as_sf() %>%
+  st_transform('+proj=longlat')
+
+rasters <-
+  list.files(path = 'data/ndvi-rasters/tapir-anna/VI_16Days_250m_v61/NDVI/',
+             pattern = '.tif', full.names = TRUE) %>%
+  stack()
+
 if(FALSE) {
-  library('raster') # to import and save rasters
-  load('data/ndvi-rasters/tapir-anna/VI_16Days_250m_v61/Time_Series/RData/Terra/NDVI/MOD13Q1_NDVI_177_2017_273_2018_RData.RData')
-  ud_poly <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.9995, level = 0) %>%
-    st_as_sf() %>%
-    st_transform('+proj=longlat')
-  raster_ts %>%
+  rasters %>%
     mask(ud_poly) %>%
     crop(ud_poly) %>%
     plot()
 }
+
+# save NDVI data as an rds file of a tibble
+#' `rasterToPoints()` on a `stack()` of the rasters fails because there's too much data
+rasters %>%
+  rasterToPoints() %>%
+  data.frame() %>%
+  pivot_longer(-c(x, y)) %>%
+  transmute(long = x,
+            lat = y,
+            date = substr(name, start = nchar('MOD13Q1_NDVI_x'), stop = nchar(name)) %>%
+              as.Date(format = '%Y_%j'),
+            ndvi = value) %>%
+  saveRDS('data/ndvi-rasters/tapir-anna/tapir-anna-data.rds')
