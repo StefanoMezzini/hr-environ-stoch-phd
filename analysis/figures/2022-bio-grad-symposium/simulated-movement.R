0c3accbf84020a68b1b0ed3b83a8d4d582460be5
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 16:03:45 -0700

    cleaned script, removed unnecessary code

--- analysis/figures/2022-bio-grad-symposium/simulated-movement.R
+++ analysis/figures/2022-bio-grad-symposium/simulated-movement.R
@@ -1,7 +1,4 @@
 library('dplyr')        # for data wrangling
-library('tidyr')        # for data wrangling
-library('sp')           # for spatial data
-library('adehabitatHR') # for minimum convex polygon HR estimation 
 library('ctmm')         # for continuous-time movement modeling
 library('ggplot2')      # for fancy plots
 library('gganimate')    # for animated fancy plots
@@ -13,9 +10,8 @@ options(gganimate.dev_args = list(width = 4, height = 4, units = 'in',
                                   res = 300))
 
 # create simulated movement ----
-m_ouf <- ctmm(tau = c(10, 1), sigma = 0.09, mu = c(0.5, 0.5), isotropic = TRUE,
-              seed = 1)
-sims_tel <- simulate(m_ouf, t = seq(1, 2000, by = 0.1))
+m_ouf <- ctmm(tau = c(10, 1), sigma = 0.09, mu = c(0.5, 0.5), isotropic = TRUE)
+sims_tel <- simulate(m_ouf, t = seq(1, 1000, by = 0.1), seed = 1)
 sims_df <- sims_tel %>%
   SpatialPoints.telemetry() %>%
   as.data.frame() %>%
@@ -23,9 +19,14 @@ sims_df <- sims_tel %>%
   mutate(t = Sys.Date() + 1:n(),
          id = 1)
 
+# static plot
+ggplot(sims_df[1:MAX, ], aes(long, lat)) +
+  coord_equal() +
+  geom_path() +
+  scale_y_reverse() # reverse y axis for aesthetics
+
 # animate only part of the track
-MAX <- nrow(sims_df)/2
-NFRAMES <- round(MAX/4)
+NFRAMES <- round(nrow(sims_df)/4)
 FPS <- NFRAMES / 30
 p_anim <-
   ggplot(sims_df[1:MAX, ], aes(long, lat)) +
@@ -33,7 +34,8 @@ p_anim <-
   geom_path() +
   scale_y_reverse() +
   transition_reveal(t)
+
 anim <- animate(p_anim, start_pause = 0, end_pause = 0, nframes = NFRAMES,
                 fps = FPS)
-anim_save(filename = 'figures/2022-bio-grad-symposium/animated-movement.gif',
-          anim); beepr::beep(2)
+
+anim_save('figures/2022-bio-grad-symposium/animated-movement.gif', anim)
