f076822a5c7412d715c614fc6bc7066ed9edac61
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 16:02:04 -0700

    using MODIStsp now, not rgee

--- functions/rescale_rename_raster.R
+++
@@ -1,6 +0,0 @@
-# function to rescale value to [-1, 1] and rename each image based on the date
-rescale_rename_raster <- function(image) {
-  date <- ee$Date(image$get('system:time_start'))$format('YYYY')
-  
-  return(image$multiply(1e-4)$select('NDVI')$rename('date'))
-}
