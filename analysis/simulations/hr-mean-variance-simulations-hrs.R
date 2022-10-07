bccc424ab1fb65f7f42f7a611f6bde1bcd143fbd
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 20:15:18 -0700

    using get_hr() now

--- analysis/simulations/hr-mean-variance-simulations-hrs.R
+++ analysis/simulations/hr-mean-variance-simulations-hrs.R
@@ -1,28 +1,39 @@
-# runtime on 32 cores with 187 GB of memory: 
-setwd('/scratch/st-mnoonan-1/stefano/hr-environ-stoch-masters') # if in sockeye
-library('ctmm')    # for continuous-time movement modeling
-library('dplyr')   # for data wrangling (e.g., %>%)
-library('purrr')   # for functional programming (e.g., map(), map_dbl())
-library('tidyr')   # for data wrangling (e.g., nested tibbles)
+library('ctmm')  # for continuous-time movement modeling
+library('dplyr') # for data wrangling (e.g., %>%)
+library('purrr') # for functional programming (e.g., map(), map_dbl())
+library('tidyr') # for data wrangling (e.g., nested tibbles)
+source('analysis/figures/mean-variance-trends-panel-data.R') # trends mu and s2
+source('functions/get_hr.R') # for gaussian HR estimate from spatial variance
 
-N <- readRDS('simulations/days-summarized.rds') %>% nrow()
+N <- nrow(d55)
+files <- list.files('analysis/sockeye-scripts/temp-files/',
+                    pattern = 'days-row*')
+
+# check how many models have fit
+length(files)
+
+# ensure all models fit
+length(files) == N
+
+# check which models didn't fit
+which(! (substr(files,
+                nchar('days-row-x'),
+                nchar(files) - nchar('.rds')) %in% 1:N))
 
 days_models <-
-  map_dfr(1:N, \(i) paste0('analysis/sockeye-scripts/temp-files/days-row', i, '.rds') %>%
-            readRDS())
+  map_dfr(list.files('analysis/sockeye-scripts/temp-files/',
+                     pattern = 'days-row-*'),
+          \(x) readRDS(paste0('analysis/sockeye-scripts/temp-files/', x)))
 
-days_models <-
+days_hrs <-
   mutate(days_models,
          # mean exploration length
          t_expl = map_dbl(tel, \(x) as.numeric(max(x$timestamp))),
          # position variance
-         pos_var = map_dbl(model, \(x) {
-           if(! is.null(x)) {
-             ctmm:::area.covm(x$sigma)
-           } else NA_real_
-         }),
+         pos_var = map_dbl(model, \(x) ctmm:::area.covm(x$sigma)),
          # calculate the HR as a function of the positional variance
-         hr_50 = -2 * log(1 - 0.50) * pi * pos_var,
-         hr_95 = -2 * log(1 - 0.95) * pi * pos_var)
+         hr_50 = get_hr(pos_var, quantile = 0.50),
+         hr_95 = get_hr(pos_var, quantile = 0.95)) %>%
+  select(-c(tel, theta, model))
 
-saveRDS(days_models, 'simulations/days-final.rds')
+saveRDS(days_hrs, 'simulations/days-hrs.rds')
