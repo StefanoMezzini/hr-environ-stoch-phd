7f3fb36ed379b138907c2544a9b8e336fd9dd76f
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 20:11:09 -0700

    removed return trips

--- analysis/simulations/hr-mean-variance-simulations-days-summarized.R
+++ analysis/simulations/hr-mean-variance-simulations-days-summarized.R
@@ -1,86 +1,77 @@
-# runtime on 32 cores with 187 GB of memory: > 4 hours
-setwd('/scratch/st-mnoonan-1/stefano/hr-environ-stoch-masters') # if in sockeye
+# runtime on 1 core with 187 GB of memory: ~ 35 min w 100 tracks, ~ 8 h w 1000 
 library('ctmm')    # for continuous-time movement modeling
 library('dplyr')   # for data wrangling (e.g., %>%)
 library('purrr')   # for functional programming (e.g., map(), map_dbl())
 library('tidyr')   # for data wrangling (e.g., nested tibbles)
+source('analysis/simulations/movement-model.R') # for consistency btwn scripts
 
-m <- ctmm(tau = c(Inf, 1), sigma = 0.1, mu = c(0, 0)) # model for return tracks
-tracks <- readRDS('simulations/labelled-tracks.rds') # simulated tracks
-types <- c('constant', 'linear', 'cyclical', 'drifting', 'erratic') # trend types
+tracks <- readRDS('simulations/tracks.rds') # simulated tracks
+types <- c('constant', 'linear', 'cyclical', 'drifting', 'erratic')# trend types
 
 days <- readRDS('simulations/days.rds')
 
-cat('Adding return trips...\n',
+cat('Making trips consecutive...\n',
     file = 'analysis/sockeye-scripts/progress.txt', append = FALSE)
 
 days_summarized <-
-  days %>% # times at which animals reached satiety on each day in each environment
+  days %>% # times at which animals reached satiety on each day in each panel
   # add labelled telemetry tracks for each day
-  left_join(readRDS('simulations/tracks.rds'), by = 'day') %>%
-  # group by panel and animal
-  group_by(mean, variance, animal) %>%
-  # add a return track to (0, 0) after satiety is reached during exploration
+  left_join(tracks, by = 'day') %>%
+  # filter tracks by when animal became full
   mutate(tel = map2(tel, t_expl,
-                    \(.tel, t_end_seconds) {
-                      # exploration path to reach satiety
-                      expl <- .tel[.tel$t <= t_end_seconds, ]
-                      # start and end of exploration path (needed for return)
-                      ends <- expl[c(1, nrow(expl)), ]
-                      # convert `expl` to data.frame to be bound to other data
-                      expl <- data.frame(expl)
-                      # add a return to the exploration so the animal loops back
-                      loop <-
-                        # return takes as long as exploration; same sampling frequency
-                        simulate(object = m, data = ends, t = unique(expl$t)) %>%
-                        # convert for data wrangling
-                        data.frame() %>%
-                        # convert for ease of use
-                        tibble() %>%
-                        # reverse time so animal returns home instead of exploring
-                        mutate(t = rev(t) + max(ends$t), path = 'return') %>%
-                        # add the exploration track
-                        bind_rows(mutate(expl, path = 'exploration')) %>%
-                        # arrange by time so exploration comes before return
-                        arrange(t)
-                      return(loop)
-                    }),
-         # find the end of each "day"
-         t_end = map_dbl(tel, \(y) max(y$t)),
-         # days end after the previous one(s)
-         t_end = cumsum(t_end),
-         # days start at end of previous one(s)
-         t_start = lag(t_end),
-         # the first day starts at time t = 0
-         t_start = if_else(is.na(t_start), 0, t_start)) %>%
-  # drop unnecessary columns
-  dplyr::select(-c(t_expl, t_end)) %>%
-  # 1 dataset / (day * animal * panel) --> a single big dataset
-  unnest(tel) %>%
+                    \(.t, .max) data.frame(.t) %>% filter(t <= .max))) %>%
+  # group by panel, animal, and day
+  group_by(mean, variance, animal, day) %>%
+  # group by panel and animal (not by day)
+  group_by(mean, variance, animal) %>%
+  mutate(t_start = lag(t_expl * 2), # start at end of previous explor. + return
+         t_start = if_else(is.na(t_start), 0, t_start), # start 1st day at t = 0
+         t_start = cumsum(t_start)) %>%
+  # drop groups
+  ungroup()
+
+#' splitting into two sections bc `unnest()` can't work with very long vectors
+N <- nrow(days_summarized)
+
+days_summarized <-
+  # expand nested datasets into a single big dataset
+  bind_rows(unnest(days_summarized[1:floor(N / 2), ], tel),
+            unnest(days_summarized[(floor(N / 2) + 1):N, ], tel)) %>%
   mutate(
-    # change t to be the date and time rather than the "time of day"
+    # change t to be the date and time of consecutive days, not "time of day"
     t = t + t_start,
     # make timestamp reflect the new values of t
-    timestamp =
-      as.POSIXct(t, tz = 'UTC',
-                 origin = as.POSIXct('1970-01-01 00:00 UTC')),
+    timestamp = as.POSIXct(t, tz = 'UTC',
+                           origin = as.POSIXct('1970-01-01 00:00 UTC')),
     # id for each animal (needed to create a telemetry object)
     individual.local.identifier = paste(mean, ' mean,',
                                         variance, 'variance,',
                                         'animal', animal)) %>%
   # drop unnecessary columns
-  select(-c(cell_id, new_cell, full, path)) %>%
+  select(-c(cell_id, new_cell, full, t_expl, t_start)) %>%
   # move the time columns to the beginning
-  relocate(c(t_start, t), .after = day) %>%
-  # a single big dataset --> 1 dataset (animal * panel), i.e., multiple days/animal now
+  relocate(t, .after = day) %>%
+  # create separated (nested) datasets for each animal in each panel
   nest(tel = -c(mean, variance, animal, mu, sigma2))
 
-
-cat('Converting data to telemetries and fiting variograms...\n',
-    file = 'analysis/sockeye-scripts/progress.txt', append = TRUE)
-days_summarized <-
-  mutate(days_summarized,
-         tel = map(tel, \(x) suppressMessages(as.telemetry(x))),
-         theta = map(tel, \(x) ctmm.guess(data = x, interactive = FALSE)))
-
 saveRDS(days_summarized, 'simulations/days-summarized.rds')
+
+if(FALSE) {
+  # diagnostics to ensure time was added up correctly
+  library('ggplot2')
+  theme_set(theme_bw())
+  
+  days_summarized <- days_summarized %>%
+    mutate(t_max = map_dbl(tel, \(x) x %>%
+                             group_by(day) %>%
+                             summarize(t_max = max(t)) %>%
+                             pull(t_max) %>%
+                             mean()),
+           t_min = map_dbl(tel, \(x) min(x$t)))
+  
+  ggplot(days_summarized) +
+    facet_grid(variance ~ mean) +
+    geom_ribbon(aes(animal, ymin = t_min, ymax = t_max), alpha = 0.3) +
+    geom_line(aes(animal, t_max), color = 'darkorange', lwd = 1) +
+    geom_line(aes(animal, t_min), color = 'darkorange', lwd = 1)
+}
