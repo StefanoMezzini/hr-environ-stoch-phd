cd45d122ae17d903748b35de64758a952385286d
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-10-06 20:16:51 -0700

    added comments, improved figures, using 1e3 tracks

--- analysis/simulations/hr-simulation-extreme-scenarios.R
+++ analysis/simulations/hr-simulation-extreme-scenarios.R
@@ -1,13 +1,13 @@
-# runtime on 1 cores with 187 GB of memory: ~ 23 hours with up to 1000 tracks
-setwd('/scratch/st-mnoonan-1/stefano/hr-environ-stoch-phd') # if in sockeye
 library('ctmm')    # for continuous-time movement modeling
 library('dplyr')   # for data wrangling (e.g., %>%)
 library('tidyr')   # for data wrangling (e.g., nested tibbles)
 library('ggplot2') # for fancy plots
 source('functions/rgamma2.R') # rgamma() parameterized by mean and variance
-source('analysis/figures/mean-variance-trends-panel-data.R') # create tibble of parameters
+source('analysis/figures/mean-variance-trends-panel-data.R') # means & variances
 source('analysis/simulations/movement-model.R') # for consistency across scripts
+source('functions/get_hr.R') # for extracting gaussian home range
 theme_set(theme_bw())
+set.seed(1) # for consistent results
 
 tels <- readRDS('simulations/tracks.rds') # list of telemetry tracks
 tracks <- readRDS('simulations/labelled-tracks.rds') # tibble of tracks
@@ -19,8 +19,6 @@ WORST <- filter(d55, mu == min(mu)) %>% # lowest mean resources
 BEST <- filter(d55, mu == max(mu)) %>% # highest mean resources
   filter(sigma2 == min(sigma2)) %>% # with lowest variance
   slice(1) # take the first row only
-
-cat('Creating tracks...\n', file = 'analysis/sockeye-scripts/progress.txt', append =FALSE)
 
 days <-
   # modify WORST and BEST to follow the syntax used in 'offspring-simulations.R'
@@ -40,21 +38,25 @@ days <-
   group_by(day, animal, scenario) %>%
   # calculate the total number of visits, total calories, and if animal is full
   mutate(satiety = cumsum(food), # use for diagnostics if animals don't get full
-         full = satiety >= REQUIRED) %>% # did the animal reach the required needs?
+         full = satiety >= REQUIRED) %>% # did the animal reach the its needs?
   filter(cumsum(full) <= 1) %>% # full only once
   ungroup()
 
-days_end <-
-  days %>%
-  group_by(scenario, day) %>%
-  filter(full, ! duplicated(full)) %>% # take the 1st row where the animal is full
-  rename(t_expl = t) %>% # to avoid duplicated colnames with tracks
-  # remove unneded columns (also avoids duplicated colnames with tracks)
-  dplyr::select(-c(x, y, vx, vy, longitude, latitude, food))
-
 if(FALSE) {
-  max(days_end$t_expl) / MAX_T # check max fraction of time used
-  sum(days_end$full) / (max(days$day) * 2) # check if all animal are full only once/day
+  # check if the ends of each day are correct and make sense
+  days_end <-
+    days %>%
+    group_by(scenario, day) %>%
+    filter(full, ! duplicated(full)) %>% # take the 1st row where animal is full
+    rename(t_expl = t) %>% # to avoid duplicated colnames with tracks
+    # remove unneded columns (also avoids duplicated colnames with tracks)
+    dplyr::select(-c(x, y, vx, vy, longitude, latitude, food))
+  
+  # check max fraction of time used (should be < 1)
+  max(days_end$t_expl) / MAX_T
+  
+  # are animals are full only once/day? (should be == 1)
+  sum(days_end$full) / (max(days$day) * 2)
   
   # plot of satiety over time by animal
   ggplot(days, aes(t, satiety, group = day)) +
@@ -80,29 +82,27 @@ if(FALSE) {
     theme(legend.position = 'top')
 }
 
-cat('Adding return trips...\n',
-    file = 'analysis/sockeye-scripts/progress.txt', append = TRUE)
-
 # single estimates that eventually converge to the asymptote ----
 days_summarized <-
   days %>%
   # find how long it took to reach satiety
   group_by(scenario, day) %>%
-  nest(day_data = -c(scenario, day)) %>%
-  mutate(t_expl = map_dbl(day_data, \(d) max(d$t))) %>%
+  nest(tel_day = -c(scenario, day)) %>%
+  mutate(t_expl = map_dbl(tel_day, \(d) max(d$t))) %>%
   # add days sequentially
   group_by(scenario) %>%
-  mutate(t_start = lag(2 * t_expl),
-         t_start = if_else(is.na(t_start), 0, t_start),
-         t_start = cumsum(t_start),
-         tel_day = map2(day, t_expl, \(i, te) tels$tel[[i]] %>%
-                          data.frame() %>%
-                          filter(t <= te))) %>%
-  unnest(tel_day) %>%
-  mutate(t = t + t_start,
-         individual.local.identifier = scenario,
-         timestamp = as.POSIXct(t, origin = '2000-01-01')) %>%
-  ungroup()
+  mutate(t_start = lag(2 * t_expl), # add the return time before next "day"
+         t_start = if_else(is.na(t_start), 0, t_start), # start at 0, not NA
+         t_start = cumsum(t_start), # make start times comsecutive
+         tel_day = map2(day, t_expl,
+                        \(i, te) tels$tel[[i]] %>% # extract tel for the day
+                          data.frame() %>% # for filtering
+                          filter(t <= te))) %>% # end tracks at satiety
+  unnest(tel_day) %>% # make one big dataset
+  mutate(t = t + t_start, # make times consecutive
+         individual.local.identifier = scenario, # ctmm identifier
+         timestamp = as.POSIXct(t, origin = '2000-01-01')) %>% # use new times 
+  ungroup() # remove grouping by scenario
 
 if(FALSE) {
   # check times are adding up correctly
@@ -115,7 +115,7 @@ if(FALSE) {
     facet_wrap(~ scenario, scales = 'free_y') +
     geom_line() +
     geom_line(aes(group = day), color = 'blue', lwd = 30) +
-    geom_line(aes(day, timestamp + t_expl, group = day), color = 'red', lwd = 30)
+    geom_line(aes(day, timestamp + t_expl, group = day), color = 'red', lwd =30)
   
   days_summarized %>%
     filter(day <= 10) %>%
@@ -124,13 +124,11 @@ if(FALSE) {
     coord_equal() +
     geom_path(aes(x, y, group = day), alpha = 0.5) +
     geom_point(aes(0, 0)) +
-    geom_point(aes(x, y), filter(days_summarized, day <= 10, t == 0), color = 'red')
+    geom_point(aes(x, y), filter(days_summarized, day <= 10, t == 0),
+               color = 'red')
 }
 
-# HR as a function of replicates days sampled ----
-cat('Creating figure of HR as a function of days sampled...\n',
-    file = 'analysis/sockeye-scripts/progress.txt', append = TRUE)
-
+# estimate saturation curve of home range size over number of days
 saturation_days <-
   expand_grid(n_days = (2^seq(1, log2(1e3), by = 0.2)) %>% round() %>% unique(),
               case = unique(days_summarized$scenario)) %>%
@@ -138,26 +136,33 @@ saturation_days <-
                      \(.n, .case) filter(days_summarized,
                                          day <= .n,
                                          scenario == .case)),
-         tel = map(data, as.telemetry),
+         tel = map(data, as.telemetry), # convert data to telemetry for modeling
          theta = map(tel, \(x) ctmm.guess(data = x, interactive = FALSE)),
-         m = map2(tel, theta, ctmm.fit),
-         sigma = map_dbl(m, \(.m) ctmm:::area.covm(.m$sigma)),
-         hr = sqrt((-2 * log(0.0001) * pi) * sigma))
+         m = map(1:n(), \(i) {
+           cat('Fitting model', i, '\n')
+           ctmm.fit(tel[[i]], theta[[i]])
+           }), # fit movement model
+         sigma = map_dbl(m, \(.m) ctmm:::area.covm(.m$sigma)), # var(position)
+         hr = get_hr(.sigma = sigma, quantile = 0.95)) # Gaussian home range
 
 saveRDS(saturation_days, 'simulations/hr-saturation-days.rds')
 
 saturation_days %>%
-  select(case, n_days, hr) %>%
-  write.csv(file = 'simulations/hr-saturation-days.csv', row.names = FALSE)
+  select(case, n_days, sigma, hr) %>%
+  readr::write_csv('simulations/hr-saturation-days.csv')
 
 p_hr_days <-
   ggplot(saturation_days, aes(n_days, hr)) +
   facet_wrap(~ case, nrow = 1) +
   geom_vline(xintercept = 100, color = 'darkorange') +
-  geom_smooth(method = 'gam', color = 'black', formula = y ~ s(x, bs = 'cs')) +
+  geom_smooth(method = 'gam', color = 'black',
+              formula = y ~ s(x, bs = 'cs', k = 10),
+              method.args = list(family = Gamma(link = 'log'))) +
   geom_point(alpha = 0.3) +
-  labs(x = 'Number of days sampled', y = 'Estimated home range') +
-  scale_x_continuous(trans = 'log2', breaks = c(2, 16, 128, 1024))
+  scale_x_continuous(expression(Number~of~days~sampled~(log[2]~scale)),
+                     trans = 'log2', breaks = c(2, 16, 128, 1024),
+                     limits = c(2, 1100)) +
+  scale_y_log10(expression(Estimated~home~range~(log[10]~scale))); p_hr_days
 
-ggsave('figures/simulations/hr-over-days.png',
-       plot = p_hr_days, width = 5, height = 3, dpi = 600)
+ggsave('figures/simulations/hr-over-days.png', plot = p_hr_days,
+       width = 6, height = 3, dpi = 'print')
