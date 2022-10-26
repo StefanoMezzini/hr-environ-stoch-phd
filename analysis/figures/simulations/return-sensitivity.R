a43a9370d642bed71292614f522f024905a54641
Stefano Mezzini <stefano@menouno.com>
2022-10-26 11:06:27 -0700

    rm duplicated proj def, add fitting time w dt=30s

--- analysis/figures/simulations/return-sensitivity.R
+++ analysis/figures/simulations/return-sensitivity.R
@@ -11,20 +11,20 @@ theme_set(theme_bw() + # change default ggplot theme
                   strip.text = element_blank()))
 
 DELTA_T <- 30 # sample every 30 s
-SAMPLES <- seq(0, 60 * 60 * 24, by = DELTA_T) # 24 hours in seconds
-PROJ <- '+proj=aeqd +lon_0=0 +lat_0=0 +datum=WGS84' # equidistant projection
+SAMPLES <- seq(0, 60 * 60 * 24, by = DELTA_T) # 24 h sampled every DELTA_T
 
 newd <- tibble(hour = 1:24, # 24 hours
                t = as.POSIXct(60 * 60 * hour, origin = '1970-01-01'),
                longitude = 0, # always at (0, 0) at the beginning of each hour
                latitude = 0,
                animal = 'test animal') %>%
-  as.telemetry(projection = PROJ)
+  as.telemetry(projection = PROJECTION)
 head(newd)
 sims_loop <- simulate(model, data = newd, dt = DELTA_T, seed = 1, complete = TRUE)
 sims_out <- sims_loop[sims_loop$t %% (60 * 60) < (60 * 30), ] # only first half hours
 sims_out <- sims_out[sims_out$t < (60 * 60 * 24), ] # remove last data point
 
+# check if `sims_loop` is continuous while `sims_out` is intermittent
 plot(rep(1, nrow(sims_loop)) ~ sims_loop$t, pch = '.', ylim = c(-1, 2))
 points(rep(0, nrow(sims_out)) ~ sims_out$t, pch = '.')
 
@@ -35,9 +35,9 @@ fit_ctmm <- function(.data) {
   tictoc::toc() # print the elapsed time
   return(model)
 }
-                              # DELTA_t     :  5 minutes,  1 minute , 30 seconds
-m_loop <- fit_ctmm(sims_loop) # Fitting time: 16 seconds, 65 seconds, 16 seconds
-m_out <- fit_ctmm(sims_out)   # Fitting time:  8 seconds, 32 seconds,  8 seconds
+                              # DELTA_t     : 5 min, 1 min,  30 s
+m_loop <- fit_ctmm(sims_loop) # Fitting time:  16 s,  65 s, 168 s
+m_out <- fit_ctmm(sims_out)   # Fitting time:   8 s,  32 s,  86 s
 
 summary(m_loop)
 summary(m_out)
