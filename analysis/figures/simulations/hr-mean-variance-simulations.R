fed0da2490aed7eff2b73c5ae5deb7168a95e198
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-11-17 13:36:41 -0800

    removed bolding for 50%, changed ylab to HR

--- analysis/figures/simulations/hr-mean-variance-simulations.R
+++ analysis/figures/simulations/hr-mean-variance-simulations.R
@@ -9,7 +9,7 @@ days <- readRDS('simulations/days-hrs.rds')
 # main simulation figure of change in home range (5x5 plot) ----
 e_r <- 'Resource abundance (\U1D53C(\U1D445))' # E(R) with blackboard bold
 v_r <- 'Resource unpredictability (\U1D54D(\U1D445))' # V(R) w blackboard bold
-hr_lab <- 'Positional variance (\U1D54D(position))' # label for y axis
+hr_lab <- 'Home range size' # label for y axis
 p_sim <-
   ggplot(days) +
   facet_grid(variance ~ mean, drop = FALSE) + # facet by trends in mu and var
@@ -28,7 +28,7 @@ p_sim <-
 p_mean <-
   ggplot(d55, aes(animal, mu)) +
   facet_grid(. ~ mean) +
-  geom_line(color = pal[1], lwd = 1) +
+  geom_line(color = pal[1], linewidth = 1) +
   scale_x_continuous(e_r, breaks = NULL, position = 'top') +
   scale_y_continuous(hr_lab, breaks = NULL) +
   theme(strip.background = element_blank(),
@@ -38,7 +38,7 @@ p_mean <-
 p_variance <-
   ggplot(d55, aes(animal, sigma2)) +
   facet_grid(variance ~ ., switch = 'y') +
-  geom_line(color = pal[2], lwd = 1) +
+  geom_line(color = pal[2], linewidth = 1) +
   scale_x_continuous('Time', breaks = NULL) +
   scale_y_continuous(v_r, breaks = NULL) +
   theme(strip.background = element_blank(),
@@ -96,33 +96,5 @@ p_full_suff
 
 # save the plot as a png
 ggsave('figures/simulations/mean-variance-5-by-5-hr-sims-sufficient.png',
-       p_full_suff, width = 8, height = 4.68, scale = 2, units = 'in',
+       p_full_suff, width = 10, height = 5.85, scale = 1, units = 'in',
        dpi = 'print', bg = 'white')
-
-# regression plots ----
-days_long <-
-  days %>%
-  pivot_longer(c(hr_50, hr_95), names_to = 'q', values_to = 'hr') %>%
-  pivot_longer(c(mu, sigma2), names_to = 'parameter', values_to = 'value') %>%
-  mutate(q = case_when(q == 'hr_50' ~ '50% home range',
-                       q == 'hr_95' ~ '95% home range') %>%
-           factor(levels = c('95% home range', '50% home range')),
-         parameter = case_when(parameter == 'mu' ~ e_r,
-                              parameter == 'sigma2' ~ v_r),
-         v_pos_lab = hr_lab)
-
-regs <-
-  ggplot(days_long) +
-  facet_grid(v_pos_lab ~ parameter, switch = 'both', scales = 'free_x') +
-  geom_point(aes(value, hr), alpha = 0.1, color = pal[3]) +
-  geom_smooth(aes(value, hr, group = q, color = parameter), se = FALSE,
-              method = 'glm', formula = y ~ x,
-              method.args = list(family = "Gamma")) +
-  scale_x_continuous(NULL, breaks = NULL) +
-  scale_y_continuous(NULL, breaks = NULL) +
-  scale_color_manual(values = pal) +
-  theme(legend.position = 'none', strip.background = element_blank(),
-        strip.text = element_text(size = 11)); regs
-
-ggsave('figures/simulations/simulation-regression-plots.png', plot = regs,
-       width = 6, height = 3, dpi = 'print', bg = 'white', scale = 1.5)
